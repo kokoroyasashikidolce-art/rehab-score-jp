58b12277799a59de6940b5836c670e0ad2c06836
Yui Kawamura <kokoroyasashikidolce@gmail.com>
2026-05-18 22:24:55 +0900

    Add SCIM3

--- src/data/scales/index.js
+++ src/data/scales/index.js
@@ -2,9 +2,11 @@ import { scimSrScale } from "./scimSr";
 import { masScale } from "./mas";
 import { fimScale } from "./fim";
 import { biScale } from "./bi";
+import { scim3Scale } from "./scim3";
 
 export const scales = [
   scimSrScale,
+  scim3Scale,
   masScale,
   fimScale,
   biScale,
